58bd7ce1fa8dfb241946f103c639dff79c46a7c3
Alina Krasavina <hnalina@gmail.com>
2018-10-11 11:54:26 +0300

    added oauthclient to setup reqs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='inapppy',
     version='0.7',
     packages=['inapppy'],
-    install_requires=['rsa', 'requests', 'google-api-python-client'],
+    install_requires=['rsa', 'requests', 'google-api-python-client', 'oauth2client==3.0.0'],
     description="In-app purchase validation library for Apple AppStore and GooglePlay.",
     keywords='in-app store purchase googleplay appstore validation',
     author='Lukas Šalkauskas',
