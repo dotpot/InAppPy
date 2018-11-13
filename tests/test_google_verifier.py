51671f2f2ee0190d4f87275bd46837b412b1b253
Ruslan Roskoshny <i.am.yarger@gmail.com>
2018-11-13 18:48:48 +0300

    Tests for google product

--- tests/test_google_verifier.py
+++ tests/test_google_verifier.py
@@ -6,15 +6,6 @@ from unittest.mock import patch
 from inapppy import googleplay, errors
 
 
-def test_google_verify_non_subscription():
-    with patch.object(googleplay, 'build', return_value=None):
-        with patch.object(googleplay.GooglePlayVerifier, '_authorize', return_value=None):
-            verifier = googleplay.GooglePlayVerifier('test-bundle-id', 'private_key_path', 30)
-            with patch.object(verifier, 'check_purchase_subscription', return_value={}):
-                with pytest.raises(NotImplementedError):
-                    verifier.verify('test-token', 'test-product')
-
-
 def test_google_verify_subscription():
     with patch.object(googleplay, 'build', return_value=None):
         with patch.object(googleplay.GooglePlayVerifier, '_authorize', return_value=None):
@@ -38,4 +29,15 @@ def test_google_verify_subscription():
 
 
 def test_google_verify_product():
-    pass
+    with patch.object(googleplay, 'build', return_value=None):
+        with patch.object(googleplay.GooglePlayVerifier, '_authorize', return_value=None):
+            verifier = googleplay.GooglePlayVerifier('test-bundle-id', 'private_key_path', 30)
+
+            # purchase
+            with patch.object(verifier, 'check_purchase_product', return_value={'purchaseState': 0}):
+                verifier.verify('test-token', 'test-product')
+
+            # cancelled
+            with patch.object(verifier, 'check_purchase_product', return_value={'purchaseState': 1}):
+                with pytest.raises(errors.GoogleError):
+                    verifier.verify('test-token', 'test-product')
