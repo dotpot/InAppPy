b0373997aa278d5506c91604a16bca6e196edb91
David Schumann <schumannd@users.noreply.github.com>
2018-12-13 17:54:29 +0100

    Fix errors not propagating raw_response

--- inapppy/errors.py
+++ inapppy/errors.py
@@ -1,11 +1,12 @@
 class InAppPyValidationError(Exception):
     """ Base class for all validation errors """
-    raw_response = None
-
     def __init__(self, *args, **kwargs):
-        self.raw_response = kwargs.pop('raw_response', None)
         super().__init__(*args, **kwargs)
 
 
 class GoogleError(InAppPyValidationError):
-    pass
+    raw_response = None
+
+    def __init__(self, message, raw_response, *args, **kwargs):
+        self.raw_response = raw_response
+        super().__init__(message, raw_response, *args, **kwargs)
