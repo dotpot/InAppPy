7e5850af2ba9535ee409aa86d8ed107669b229ee
Lukas <halfas.online@gmail.com>
2019-01-25 12:58:33 +0200

    version bump 2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="inapppy",
-    version="2.2",
+    version="2.3",
     packages=["inapppy", "inapppy.asyncio"],
     install_requires=[
         "aiohttp",
