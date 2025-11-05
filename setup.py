9aff6ca33ca571c9705af3db96b2dd56f6b532e5
Lukas Šalkauskas <halfas.online@gmail.com>
2025-11-05 13:08:54 +0200

    fix openssl module issue

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name="inapppy",
     version="2.6",
     packages=["inapppy", "inapppy.asyncio"],
-    install_requires=["aiohttp", "rsa", "requests", "google-api-python-client", "oauth2client"],
+    install_requires=["aiohttp", "rsa", "requests", "google-api-python-client", "oauth2client", "pyOpenSSL<=24.2.1"],
     description="In-app purchase validation library for Apple AppStore and GooglePlay.",
     keywords="in-app store purchase googleplay appstore validation",
     author="Lukas Šalkauskas",
